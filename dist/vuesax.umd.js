253dd9ba1ad50f214d1a3d748b0bfc6b26c77560
Sunsern Cheamanunkul <sunsern.che@mahidol.edu>
2020-07-23 02:43:34 +0700

    rearrange order

--- dist/vuesax.umd.js
+++ dist/vuesax.umd.js
@@ -13362,12 +13362,12 @@ var vsDropDownGroup_component = normalizeComponent(
   Vue.component(vsDropDownItem.name, vsDropDownItem);
   Vue.component(vsDropDownGroup.name, vsDropDownGroup);
 });
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"4dbce354-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/vsTable/vsTable.vue?vue&type=template&id=769edc0a&
-var vsTablevue_type_template_id_769edc0a_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"vs-component vs-con-table",class:[{'stripe': _vm.stripe, 'hoverFlat': _vm.hoverFlat}, ("vs-table-" + _vm.color)]},[_c('header',{staticClass:"header-table vs-table--header"},[_vm._t("header"),(_vm.search)?_c('div',{staticClass:"con-input-search vs-table--search"},[_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.searchx),expression:"searchx"}],staticClass:"input-search vs-table--search-input",attrs:{"type":"text"},domProps:{"value":(_vm.searchx)},on:{"input":function($event){if($event.target.composing){ return; }_vm.searchx=$event.target.value}}}),_c('vs-icon',{attrs:{"icon":"search"}})],1):_vm._e()],2),_c('div',{staticClass:"con-tablex vs-table--content"},[_c('div',{staticClass:"vs-con-tbody vs-table--tbody ",style:(_vm.styleConTbody)},[_c('table',{ref:"table",staticClass:"vs-table vs-table--tbody-table"},[_c('thead',{ref:"thead",staticClass:"vs-table--thead"},[_c('tr',[(_vm.multiple || _vm.hasExpadableData)?_c('th',{staticClass:"td-check"},[(_vm.multiple)?_c('span',{staticClass:"con-td-check"},[_c('vs-checkbox',{key:_vm.isCheckedLine ? 'remove' : 'check',attrs:{"icon":_vm.isCheckedLine ? 'remove' : 'check',"checked":_vm.isCheckedMultiple,"size":"small"},on:{"change":_vm.changeCheckedMultiple}})],1):_vm._e()]):_vm._e(),_vm._t("thead")],2)]),_vm._t("default",null,{"data":_vm.datax})],2)]),(_vm.isNoData)?_c('div',{staticClass:"not-data-table vs-table--not-data"},[_vm._v("\n      "+_vm._s(_vm.noDataText)+"\n    ")]):_vm._e(),(_vm.pagination)?_c('div',{staticClass:"con-pagination-table vs-table--pagination"},[_c('vs-pagination',{attrs:{"total":_vm.searchx && !_vm.sst ? _vm.getTotalPagesSearch : _vm.getTotalPages,"description-items":_vm.descriptionItems,"max-items":_vm.maxItemsx,"size-array":_vm.sst ? _vm.total : _vm.queriedResults.length,"description":_vm.description,"description-title":_vm.descriptionTitle,"description-connector":_vm.descriptionConnector,"description-body":_vm.descriptionBody},on:{"changeMaxItems":_vm.changeMaxItems},model:{value:(_vm.currentx),callback:function ($$v) {_vm.currentx=$$v},expression:"currentx"}})],1):_vm._e()])])}
-var vsTablevue_type_template_id_769edc0a_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"4dbce354-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/vsTable/vsTable.vue?vue&type=template&id=5e66f9e3&
+var vsTablevue_type_template_id_5e66f9e3_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"vs-component vs-con-table",class:[{'stripe': _vm.stripe, 'hoverFlat': _vm.hoverFlat}, ("vs-table-" + _vm.color)]},[_c('header',{staticClass:"header-table vs-table--header"},[_vm._t("header"),(_vm.search)?_c('div',{staticClass:"con-input-search vs-table--search"},[_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.searchx),expression:"searchx"}],staticClass:"input-search vs-table--search-input",attrs:{"type":"text"},domProps:{"value":(_vm.searchx)},on:{"input":function($event){if($event.target.composing){ return; }_vm.searchx=$event.target.value}}}),_c('vs-icon',{attrs:{"icon":"search"}})],1):_vm._e()],2),_c('div',{staticClass:"con-tablex vs-table--content"},[_c('div',{staticClass:"vs-con-tbody vs-table--tbody",style:(_vm.styleConTbody)},[_c('table',{ref:"table",staticClass:"vs-table vs-table--tbody-table"},[_c('thead',{ref:"thead",staticClass:"vs-table--thead"},[_c('tr',[(_vm.multiple || _vm.hasExpadableData)?_c('th',{staticClass:"td-check"},[(_vm.multiple)?_c('span',{staticClass:"con-td-check"},[_c('vs-checkbox',{key:_vm.isCheckedLine ? 'remove' : 'check',attrs:{"icon":_vm.isCheckedLine ? 'remove' : 'check',"checked":_vm.isCheckedMultiple,"size":"small"},on:{"change":_vm.changeCheckedMultiple}})],1):_vm._e()]):_vm._e(),_vm._t("thead")],2)]),_vm._t("default",null,{"data":_vm.datax})],2)]),(_vm.isNoData)?_c('div',{staticClass:"not-data-table vs-table--not-data"},[_vm._v(_vm._s(_vm.noDataText))]):_vm._e(),(_vm.pagination)?_c('div',{staticClass:"con-pagination-table vs-table--pagination"},[_c('vs-pagination',{attrs:{"total":_vm.searchx && !_vm.sst ? _vm.getTotalPagesSearch : _vm.getTotalPages,"description-items":_vm.descriptionItems,"max-items":_vm.maxItemsx,"size-array":_vm.sst ? _vm.total : _vm.queriedResults.length,"description":_vm.description,"description-title":_vm.descriptionTitle,"description-connector":_vm.descriptionConnector,"description-body":_vm.descriptionBody},on:{"changeMaxItems":_vm.changeMaxItems},model:{value:(_vm.currentx),callback:function ($$v) {_vm.currentx=$$v},expression:"currentx"}})],1):_vm._e()])])}
+var vsTablevue_type_template_id_5e66f9e3_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/vsTable/vsTable.vue?vue&type=template&id=769edc0a&
+// CONCATENATED MODULE: ./src/components/vsTable/vsTable.vue?vue&type=template&id=5e66f9e3&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.reduce.js
 var es_array_reduce = __webpack_require__("13d5");
@@ -13467,36 +13467,16 @@ function _typeof(obj) {
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ var vsTablevue_type_script_lang_js_ = ({
   name: "VsTable",
   props: {
     value: {},
     color: {
-      default: 'primary',
+      default: "primary",
       type: String
     },
     noDataText: {
-      default: 'No data Available',
+      default: "No data Available",
       type: String
     },
     stripe: {
@@ -13508,7 +13488,7 @@ function _typeof(obj) {
       type: Boolean
     },
     maxHeight: {
-      default: 'auto',
+      default: "auto",
       type: String
     },
     multiple: {
@@ -13545,15 +13525,15 @@ function _typeof(obj) {
       type: Array
     },
     descriptionTitle: {
-      default: 'Registries',
+      default: "Registries",
       type: String
     },
     descriptionConnector: {
-      default: 'of',
+      default: "of",
       type: String
     },
     descriptionBody: {
-      default: 'Pages',
+      default: "Pages",
       type: String
     },
     currentPage: {
@@ -13575,7 +13555,7 @@ function _typeof(obj) {
   },
   data: function data() {
     return {
-      headerWidth: '100%',
+      headerWidth: "100%",
       trs: [],
       datax: [],
       searchx: null,
@@ -13627,7 +13607,7 @@ function _typeof(obj) {
     styleConTbody: function styleConTbody() {
       return {
         maxHeight: this.maxHeight,
-        overflow: this.maxHeight != 'auto' ? 'auto' : null
+        overflow: this.maxHeight != "auto" ? "auto" : null
       };
     },
     getThs: function getThs() {
@@ -13648,14 +13628,13 @@ function _typeof(obj) {
     },
     currentx: function currentx() {
       if (this.sst) {
-        this.$emit('change-page', this.currentx);
+        this.$emit("change-page", this.currentx);
       } else {
         this.loadData();
       }
     },
     maxItems: function maxItems(val) {
-      this.maxItemsx = val;
-      this.loadData();
+      this.maxItemsx = val; //this.loadData();
     },
     maxItemsx: function maxItemsx() {
       this.loadData();
@@ -13672,7 +13651,7 @@ function _typeof(obj) {
     },
     searchx: function searchx() {
       if (this.sst) {
-        this.$emit('search', this.searchx);
+        this.$emit("search", this.searchx);
       } else {
         this.loadData();
         this.currentx = 1;
@@ -13680,16 +13659,16 @@ function _typeof(obj) {
     }
   },
   mounted: function mounted() {
-    window.addEventListener('resize', this.listenerChangeWidth);
-    this.maxItemsx = this.maxItems;
-    this.loadData(); // this.$nextTick(() => {
+    window.addEventListener("resize", this.listenerChangeWidth);
+    this.maxItemsx = this.maxItems; //this.loadData();
+    // this.$nextTick(() => {
     //   if(this.datax.length > 0) {
     //     this.changeTdsWidth()
     //   }
     // })
   },
   destroyed: function destroyed() {
-    window.removeEventListener('resize', this.listenerChangeWidth);
+    window.removeEventListener("resize", this.listenerChangeWidth);
   },
   methods: {
     loadData: function loadData() {
@@ -13717,8 +13696,8 @@ function _typeof(obj) {
           currentSortType = this.currentSortType;
 
       function compare(a, b) {
-        if (a[currentSortKey] < b[currentSortKey]) return currentSortType == 'desc' ? 1 : -1;
-        if (a[currentSortKey] > b[currentSortKey]) return currentSortType == 'desc' ? -1 : 1;
+        if (a[currentSortKey] < b[currentSortKey]) return currentSortType == "desc" ? 1 : -1;
+        if (a[currentSortKey] > b[currentSortKey]) return currentSortType == "desc" ? -1 : 1;
         return 0;
       }
 
@@ -13739,24 +13718,24 @@ function _typeof(obj) {
       this.currentSortType = sortType;
 
       if (this.sst) {
-        this.$emit('sort', key, sortType);
+        this.$emit("sort", key, sortType);
         return;
       }
 
       this.loadData();
     },
     normalize: function normalize(string) {
-      return string.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+      return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
     },
     getValues: function getValues(obj) {
       function flattenDeep(val) {
         return Object.values(val || []).reduce(function (acc, val) {
-          return _typeof(val) === 'object' ? acc.concat(flattenDeep(val)) : acc.concat(val);
+          return _typeof(val) === "object" ? acc.concat(flattenDeep(val)) : acc.concat(val);
         }, []);
       }
 
       return flattenDeep(obj).filter(function (item) {
-        return typeof item === 'string' || typeof item === 'number';
+        return typeof item === "string" || typeof item === "number";
       });
     },
     changeCheckedMultiple: function changeCheckedMultiple() {
@@ -13765,9 +13744,9 @@ function _typeof(obj) {
       var selectedx = lengthx - lengthSelected;
 
       if (selectedx == 0) {
-        this.$emit('input', []);
+        this.$emit("input", []);
       } else {
-        this.$emit('input', this.data);
+        this.$emit("input", this.data);
       }
     },
     handleCheckbox: function handleCheckbox(tr) {
@@ -13780,8 +13759,8 @@ function _typeof(obj) {
           val.push(tr);
         }
 
-        this.$emit('input', val);
-        this.$emit('selected', tr);
+        this.$emit("input", val);
+        this.$emit("selected", tr);
       }
     },
     clicktr: function clicktr(tr, isTr) {
@@ -13794,16 +13773,16 @@ function _typeof(obj) {
           val.push(tr);
         }
 
-        this.$emit('input', val);
-        this.$emit('selected', tr);
+        this.$emit("input", val);
+        this.$emit("selected", tr);
       } else if (isTr && !this.onlyClickCheckbox) {
-        this.$emit('input', tr);
-        this.$emit('selected', tr);
+        this.$emit("input", tr);
+        this.$emit("selected", tr);
       }
     },
     dblclicktr: function dblclicktr(tr, isTr) {
       if (isTr) {
-        this.$emit('dblSelection', tr);
+        this.$emit("dblSelection", tr);
       }
     },
     listenerChangeWidth: function listenerChangeWidth() {
@@ -13812,12 +13791,12 @@ function _typeof(obj) {
     },
     changeTdsWidth: function changeTdsWidth() {
       if (!this.value) return;
-      var tbody = this.$refs.table.querySelector('tbody'); // Adding condition removes querySelector none error - if tbody isnot present
+      var tbody = this.$refs.table.querySelector("tbody"); // Adding condition removes querySelector none error - if tbody isnot present
 
       if (tbody) {
-        var trvs = tbody.querySelector('.tr-values');
+        var trvs = tbody.querySelector(".tr-values");
         if (trvs === undefined || trvs === null) return;
-        var tds = trvs.querySelectorAll('.td');
+        var tds = trvs.querySelectorAll(".td");
         var tdsx = [];
         tds.forEach(function (td, index) {
           tdsx.push({
@@ -13828,19 +13807,19 @@ function _typeof(obj) {
         var colgrouptable = this.$refs.colgrouptable;
 
         if (colgrouptable !== undefined && colgrouptable !== null) {
-          var colsTable = colgrouptable.querySelectorAll('.col');
+          var colsTable = colgrouptable.querySelectorAll(".col");
           colsTable.forEach(function (col, index) {
-            col.setAttribute('width', tdsx[index].widthx);
+            col.setAttribute("width", tdsx[index].widthx);
           });
         }
       }
     },
     changeMaxItems: function changeMaxItems(index) {
-      this.maxItemsx = this.descriptionItems[index];
-
       if (this.sst) {
-        this.$emit('change-max-size', this.maxItemsx);
+        this.$emit("change-max-size", this.descriptionItems[index]);
       }
+
+      this.maxItemsx = this.descriptionItems[index];
     }
   }
 });
@@ -13856,8 +13835,8 @@ function _typeof(obj) {
 
 var vsTable_component = normalizeComponent(
   vsTable_vsTablevue_type_script_lang_js_,
-  vsTablevue_type_template_id_769edc0a_render,
-  vsTablevue_type_template_id_769edc0a_staticRenderFns,
+  vsTablevue_type_template_id_5e66f9e3_render,
+  vsTablevue_type_template_id_5e66f9e3_staticRenderFns,
   false,
   null,
   null,
